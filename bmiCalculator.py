702016059ae7a826e5443b9fcbdf130dbe48a233
Madondo07 <dumisanemadondo926@gmail.com>
2025-03-31 23:40:44 +0200

    Convertors

---
+++ bmiCalculator.py
@@ -0,0 +1,16 @@
+weight = float(input("Weight:"))
+
+# Loop until the user provides a valid unit
+while True:
+    unit = input("K(g) or L(bs):").upper()
+    if unit in ["K", "L"]:
+        break
+    print("Invalid input! Please enter 'K' for Kilogram and 'L' for Pounds.")
+
+# Perform conversion based on valid input
+if unit.upper() == "K":
+    converted = weight/0.45
+    print("Weight in L(bs): " + "{:.2f}".format(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in K(g): " + "{:.2f}".format(converted))
